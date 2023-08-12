28f97dd558e7d36cdedb88d421d1a0e60063c39b
Isha Singh <isha080400@gmail.com>
2023-08-12 12:53:11 +0530

    [Isha] Add: code for solving the angry birds problem

---
+++ binarySearch/angryBirds.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int findMaxMinDistance(vector<int> nests, int numOfBirds, int numOfNests) {
+    int ans, s = 0, e = (nests[nests.size() - 1] - nests[0]), mid;
+
+    while(s <= e) {
+        mid = (s + e) / 2;
+
+        int found = 1, curr = 0;
+
+        for(int i = 1; i < nests.size(); i++) {
+            if((nests[i] - nests[curr]) >= mid) {
+                curr = i;
+                found++;
+            }
+        }
+
+        if(found >= numOfBirds) {
+            ans = mid;
+            s = (mid + 1);
+        }
+        else{
+            e = (mid - 1);
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    vector<int> nests = {1, 2, 4, 8, 9};
+    int numOfNests = 5;
+    int numOfBirds = 3;
+
+    cout << findMaxMinDistance(nests, numOfBirds, numOfNests) << endl;
+    return 0;
+}

05194f1d8e32fa1b51d697df56646be7be1b71bd
Isha Singh <isha080400@gmail.com>
2023-08-12 11:53:19 +0530

    [Isha] Fix: the code for wrong output

--- binarySearch/squareRoot.cpp
+++ binarySearch/squareRoot.cpp
@@ -5,6 +5,7 @@ float square_root(int n, int p) {
     int s = 0, e = n, mid;
     float ans;
 
+    // finding the integer part
     while(s <= e) {
         mid = (s + e) / 2;
 
@@ -20,26 +21,26 @@ float square_root(int n, int p) {
         }
     }
 
+    // finding the decimal part
     int divisor = 10;
     while(p--) {
-        float temp;
-
+        float temp2;
         for(int i = 0; i < 10; i++) {
-            temp = (ans + (float)i / divisor);
+            float temp = (ans + ((float)i / divisor));
 
             if((temp * temp) <= n) {
-                ans = temp;
+                temp2 = temp;
             }
         }
         divisor *= 10;
-        cout << "ans " << ans << endl;
+        ans = temp2;
     }
 
     return ans;
 }
 
 int main() {
-    cout << square_root(10, 4);
+    cout << square_root(10, 5);
 
     return 0;
 }

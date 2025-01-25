5c13d043f88a459a292ed437a712016af0e90682
Isha Singh <isha080400@gmail.com>
2025-01-25 19:13:27 +0530

    [Isha] Solve: BFS traversal of a graph

---
+++ graph/bfs.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+#include<list>
+#include<queue>
+using namespace std;
+
+class Graph {
+    int v;
+    list<int> *vertices;
+
+public:
+    Graph(int v) {
+        this->v = v;
+        vertices = new list<int>[v];
+    }
+
+    void addEdge(int i, int j, bool isDirected = false) {
+        vertices[i].push_back(j);
+
+        if(!isDirected) {
+            vertices[j].push_back(i);
+        }
+    }
+
+    void printList() {
+        for(int i = 0; i < v; i++) {
+            cout << i << " -> ";
+
+            for(int elem: vertices[i]) {
+                cout << elem << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    void bfs(int source) {
+        queue<int> q;
+        bool *visited = new bool[v]{0};
+
+        q.push(source);
+        visited[source] = true;
+
+        while(!q.empty()) {
+            int s = q.size();
+
+            for(int i = 0; i < s; i++) {
+                int f = q.front();
+                cout << f << " ";
+                q.pop();
+
+                for(auto nbrs: vertices[f]) {
+                    if(!visited[nbrs]) {
+                        q.push(nbrs);
+                        visited[nbrs] = true;
+                    }
+                }
+            }
+            cout << endl;
+        }
+    }
+};
+
+
+int main () {
+    Graph *g = new Graph(7);
+    g->addEdge(0, 1);
+    g->addEdge(0, 4);
+    g->addEdge(2, 1);
+    g->addEdge(3, 4);
+    g->addEdge(4, 5);
+    g->addEdge(2, 3);
+    g->addEdge(3, 5);
+    g->addEdge(5, 6);
+
+    g->printList();
+    g->bfs(1);
+    return 0;
+}
